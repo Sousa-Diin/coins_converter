150d51c611485984248d1242d11b0e1eb4499716
Wildes Sousa <74382948+Sousa-Diin@users.noreply.github.com>
2022-11-12 13:49:56 -0300

    :rocket: add api | storage

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-home',
@@ -7,46 +8,63 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  valor : number;
+  valor : number = null;
   dConv : string;
   pConv : string;
   resultado : number = 0;
+   
 
   constructor() {
     console.log(this.valor,this.dConv, this.pConv, this.resultado);
-
+ 
   }
 
   handleConverter(){
-    const dolar = 5.33;
+    const url = environment.api;
+    const coins = 'USD-BRL,EUR-BRL';
+
     const real = 1.0;
-    const euro = 5.52;
-
+    let dolar = 0;let euro = 0;
     let resul = 0;
-    //console.log(resul);
+    
+    fetch(url + coins)
+      .then(function(response){
+        return response.json();
+      })
 
-    if (this.dConv == 'Real(BRL)' ){
-      if(this.pConv == 'Dollar(USD)'){
-        resul = (real/dolar);
-      }else if ( this.pConv == 'Euro(EUR)'){
-        resul = real/euro;
-      }
-      
-    }else if (this.dConv == 'Dollar(USD)'){
-      if(this.pConv == 'Real(BRL)'){
-        resul = dolar/real;
-      }else if ( this.pConv == 'Euro(EUR)'){
-        resul = dolar/euro;
-      }
-    }else if (this.dConv == 'Euro(EUR)'){
-      if(this.pConv == 'Dollar(USD)'){
-        resul = euro/dolar;
-      }else if ( this.pConv == 'Real(BRL)'){
-        resul = euro/real;
-      }
-    } 
-    this.resultado = this.valor * resul;
+      .then(function(data){
+        console.log(data.USDBRL.bid)
+        dolar = data.USDBRL.bid;
+        localStorage.setItem('euro', data.EURBRL.bid);
+        localStorage.setItem('dolar', dolar.toString());
 
+      })
+
+      dolar = parseFloat(localStorage.getItem('dolar'));
+      euro = parseFloat(localStorage.getItem('euro'))
+      if (this.dConv == 'Real(BRL)' ){
+        if(this.pConv == 'Dollar(USD)'){
+          resul = (real/dolar);
+        }else if ( this.pConv == 'Euro(EUR)'){
+          resul = real/euro;
+        }
+        
+      }else if (this.dConv == 'Dollar(USD)'){
+        if(this.pConv == 'Real(BRL)'){
+          resul = dolar/real;
+        }else if ( this.pConv == 'Euro(EUR)'){
+          resul = dolar/euro;
+        }
+      }else if (this.dConv == 'Euro(EUR)'){
+        if(this.pConv == 'Dollar(USD)'){
+          resul = euro/dolar;
+        }else if ( this.pConv == 'Real(BRL)'){
+          resul = euro/real;
+        }
+      } 
+  
+      this.resultado = this.valor * resul;
+      console.log(this.resultado)
   }
 
   handleChange(){
