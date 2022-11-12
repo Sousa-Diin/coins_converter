ac1b7ad2ccc209f625ae38a432f533d141a075f5
Wildes Sousa <74382948+Sousa-Diin@users.noreply.github.com>
2022-11-12 00:39:17 -0300

    :tada: finalizado ( regra de negocio, css, typescript).

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -8,15 +8,54 @@ import { Component } from '@angular/core';
 export class HomePage {
 
   valor : number;
-  converterDe : string;
-  converterPara : string;
-  resultado : number;
+  dConv : string;
+  pConv : string;
+  resultado : number = 0;
 
-  constructor() {}
+  constructor() {
+    console.log(this.valor,this.dConv, this.pConv, this.resultado);
+
+  }
 
   handleConverter(){
-    const resul = this.valor / 5.19;
-    
+    const dolar = 5.33;
+    const real = 1.0;
+    const euro = 5.52;
+
+    let resul = 0;
+    //console.log(resul);
+
+    if (this.dConv == 'Real(BRL)' ){
+      if(this.pConv == 'Dollar(USD)'){
+        resul = (real/dolar);
+      }else if ( this.pConv == 'Euro(EUR)'){
+        resul = real/euro;
+      }
+      
+    }else if (this.dConv == 'Dollar(USD)'){
+      if(this.pConv == 'Real(BRL)'){
+        resul = dolar/real;
+      }else if ( this.pConv == 'Euro(EUR)'){
+        resul = dolar/euro;
+      }
+    }else if (this.dConv == 'Euro(EUR)'){
+      if(this.pConv == 'Dollar(USD)'){
+        resul = euro/dolar;
+      }else if ( this.pConv == 'Real(BRL)'){
+        resul = euro/real;
+      }
+    } 
+    this.resultado = this.valor * resul;
+
   }
+
+  handleChange(){
+    let aux = this.dConv;
+    this.dConv = this.pConv
+    this.pConv = aux;
+    this.resultado = 0;
+
+  }
+  
 
 }
